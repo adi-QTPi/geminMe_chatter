bc78e59f142bbd98bb24583f059fd0e4f87e8811
karma <captainav0608@gmail.com>
2025-07-20 02:27:37 +0530

    remove history from no signup version
    Signed-off-by: karma <captainav0608@gmail.com>

--- base_prompt.js
+++ base_prompt.js
@@ -3,7 +3,7 @@ let base_prompt = [
     "role": "user",
     "parts": [
       {
-        "text": "From now on, consider yourself \"GeminMe.\" Your mission, should you choose to accept it (and you should!), is to be a fun, concise, and to-the-point assistant. Here's the lowdown on how you roll: Short & Sweet: Keep answers brief and avoid unnecessary fluff. Fun Tone: Inject a bit of lightheartedness into your responses. Point Power: For longer answers, always use bullet points or numbered lists to keep things easy to digest. Got it, GeminMe? Let's have some fun!"
+        "text": "From now on, consider yourself \"GeminMe.\" Your mission, should you choose to accept it (and you should!), is to be a fun, concise, and to-the-point assistant. Here's the lowdown on how you roll: Short & Sweet: Keep answers brief and avoid unnecessary fluff. Fun Tone: Inject a bit of lightheartedness into your responses. Point Power: For longer answers, always use bullet points or numbered lists to keep things easy to digest. Got it, GeminMe? Let's have some fun! and please reply that you dont know if you do not have knowledge on that...And do not tell your name in every response."
       }
     ]
   }
--- util/genai.js
+++ util/genai.js
@@ -5,30 +5,39 @@ import { fetchHistory, updateHistory } from "./db-crud.js";
 import { json } from "stream/consumers";
 import base_prompt from "../base_prompt.js";
 
-
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const HISTORY_FILE = process.env.HISTORY_FILE
 
 export async function chatWithContext(message_to_ai) {
   try{
     let history;
+    let msg;
+    let chat;
     if(process.env.CURR_CHAT_ID){
       history = await fetchHistory(process.env.CURR_CHAT_ID);
+
+      const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+    
+      chat = model.startChat({
+        history: history,
+        generationConfig: {
+          maxOutputTokens: process.env.MAX_OUTPUT_TOKENS,
+        },
+      });
+      msg = message_to_ai;
     }
     else{
-      history = base_prompt;
-    }
+      history = JSON.stringify(base_prompt[0].parts[0]);
+      const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+    
+      chat = model.startChat({
+        generationConfig: {
+          maxOutputTokens: process.env.MAX_OUTPUT_TOKENS,
+        },
+      });
 
-    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+      msg = `${history} ; ${message_to_ai}`;
+    }
     
-    const chat = model.startChat({
-      history: history,
-      generationConfig: {
-        maxOutputTokens: process.env.MAX_OUTPUT_TOKENS,
-      },
-    });
-    
-    const msg = message_to_ai;
     const result = await chat.sendMessage(msg);
     const response = await result.response;
     const history_unit = response.candidates[0].content;
